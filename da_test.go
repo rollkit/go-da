003ba5a94b62de12fe950267b3b0f552b1ad46bf
Tomasz Zdybał <tomek@zdybal.lap.pl>
2023-09-18 11:14:08 +0200

    test: DummyDA implementation and DA interface test

---
+++ da_test.go
@@ -0,0 +1,62 @@
+package da_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/rollkit/go-da"
+)
+
+func TestDummyDA(t *testing.T) {
+	dummy := NewDummyDA()
+	ExecuteDATest(t, dummy)
+}
+
+func ExecuteDATest(t *testing.T, da da.DA) {
+	msg1 := []byte("message 1")
+	msg2 := []byte("message 2")
+
+	id1, proof1, err := da.Submit(msg1)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id1)
+	assert.NotEmpty(t, proof1)
+
+	id2, proof2, err := da.Submit(msg2)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id2)
+	assert.NotEmpty(t, proof2)
+
+	id3, proof3, err := da.Submit(msg1)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id3)
+	assert.NotEmpty(t, proof3)
+
+	assert.NotEqual(t, id1, id2)
+	assert.NotEqual(t, id1, id3)
+
+	ret, err := da.Get(id1)
+	assert.NoError(t, err)
+	assert.Equal(t, msg1, ret)
+
+	commitment1, err := da.Commit(msg1)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, commitment1)
+
+	commitment2, err := da.Commit(msg2)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, commitment2)
+
+	ok, err := da.Validate(commitment1, proof1)
+	assert.NoError(t, err)
+	assert.True(t, ok)
+
+	ok, err = da.Validate(commitment1, proof2)
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+	ok, err = da.Validate(commitment2, proof1)
+	assert.NoError(t, err)
+	assert.False(t, ok)
+
+}
---
+++ dummy_test.go
@@ -0,0 +1,66 @@
+package da_test
+
+import (
+	"crypto/ed25519"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/binary"
+	"github.com/rollkit/go-da"
+	"sync/atomic"
+)
+
+// DummyDA is a simple implementation of in-memory DA. Not production ready! Intended only for testing!
+//
+// Data is stored in a map, where key is a serialized sequence number. This key is returned as ID.
+// Commitments are simply hashes, and proofs are ED25519 signatures.
+type DummyDA struct {
+	data    map[string][]byte
+	privKey ed25519.PrivateKey
+	pubKey  ed25519.PublicKey
+	cnt     uint64
+}
+
+func NewDummyDA() *DummyDA {
+	da := &DummyDA{
+		data: make(map[string][]byte),
+	}
+	da.pubKey, da.privKey, _ = ed25519.GenerateKey(rand.Reader)
+	return da
+}
+
+func (d *DummyDA) Get(id da.ID) (da.Blob, error) {
+	return d.data[string(id)], nil
+}
+
+func (d *DummyDA) Commit(blob da.Blob) (da.Commitment, error) {
+	return d.getHash(blob), nil
+}
+
+func (d *DummyDA) Submit(blob da.Blob) (da.ID, da.Proof, error) {
+	id := d.nextID()
+	proof := d.getProof(id, blob)
+
+	d.data[string(id)] = blob
+	return id, proof, nil
+}
+
+func (d *DummyDA) Validate(commit da.Commitment, proof da.Proof) (bool, error) {
+	return ed25519.Verify(d.pubKey, commit, proof), nil
+}
+
+func (d *DummyDA) nextID() []byte {
+	cnt := atomic.AddUint64(&d.cnt, 1)
+	id := make([]byte, 8)
+	binary.LittleEndian.PutUint64(id, cnt)
+	return id
+}
+
+func (d *DummyDA) getHash(blob []byte) []byte {
+	sha := sha256.Sum256(blob)
+	return sha[:]
+}
+
+func (d *DummyDA) getProof(id, blob []byte) []byte {
+	sign, _ := d.privKey.Sign(rand.Reader, d.getHash(blob), &ed25519.Options{})
+	return sign
+}
