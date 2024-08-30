04d49461d2304f6da3a20fa5c224f045e45f9f88
Yarik Bratashchuk <yarik.bratashchuk@gmail.com>
2024-08-30 13:14:18 +0000

    Fix test

--- test/test_suite.go
+++ test/test_suite.go
@@ -3,7 +3,6 @@ package test
 import (
 	"bytes"
 	"context"
-	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -138,9 +137,7 @@ func ConcurrentReadWriteTest(t *testing.T, d da.DA) {
 		defer wg.Done()
 		for i := uint64(1); i <= 100; i++ {
 			_, err := d.GetIDs(ctx, i, []byte{})
-			if err != nil && !strings.Contains(err.Error(), ErrNoBlobAtHeight.Error()) {
-				assert.NoError(t, err)
-			}
+			assert.NoError(t, err)
 		}
 	}()
 
